b5e3b35f32bde556edaa0bc9b5e1902337947d72
ShiWei Hong <hswhong96@gmail.com>
2023-02-23 14:01:21 +0000

    Seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open("#{url}").read)
+
+puts "Seeding movies"
+
+Movie.destroy_all
+
+puts "Destroying previously created movies!"
+
+movies['results'].take(10).each do |movie|
+  Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Movies successfully seeded!"
